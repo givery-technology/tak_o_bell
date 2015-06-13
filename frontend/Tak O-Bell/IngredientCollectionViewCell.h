a64ff54092fa477a86c1a13b49f43b2ada094a7b
Albert Le <AlbeeeLe@Alberts-MacBook-Pro.local>
2015-06-13 16:43:00 +0900

    Added IngredientCell

---
+++ frontend/Tak O-Bell/IngredientCollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  IngredientCollectionViewCell.h
+//  Tak O-Bell
+//
+//  Created by Albert Le on 2015-06-13.
+//  Copyright (c) 2015 Tak O-Bell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IngredientCollectionViewCell : UICollectionViewCell
+
+@property (nonatomic, strong) NSString *imageName;
+
+@end
