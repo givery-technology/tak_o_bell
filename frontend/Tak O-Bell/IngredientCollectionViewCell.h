110b441767b83185f394bf6f82106c339d4ca7bd
Albert Le <AlbeeeLe@Alberts-MacBook-Pro.local>
2015-06-13 15:52:09 +0900

    Created new views

---
+++ frontend/Tak O-Bell/IngredientCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  IngredientCollectionViewCell.h
+//  Tak O-Bell
+//
+//  Created by Albert Le on 2015-06-13.
+//  Copyright (c) 2015 Tak O-Bell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IngredientCollectionViewCell : UICollectionViewCell
+
+@end
