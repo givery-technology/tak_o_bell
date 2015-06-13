9a160fb05737f89ebbd7e76213792e763274e06b
Brian Quach <bquach88@gmail.com>
2015-06-13 18:12:33 +0900

    Create camera overlay.

--- frontend/Tak O-Bell/CameraViewController.m
+++ frontend/Tak O-Bell/CameraViewController.m
@@ -10,13 +10,43 @@
 
 @interface CameraViewController ()
 
+@property (nonatomic) IBOutlet UIView *overlayView;
+@property (nonatomic, weak) IBOutlet UIButton *takePictureButton;
+@property (nonatomic, weak) IBOutlet UIButton *returnToPreferencesButton;
+@property (nonatomic) UIImagePickerController *imagePickerController;
+
 @end
 
 @implementation CameraViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    UIImagePickerControllerSourceType sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.modalPresentationStyle = UIModalPresentationCurrentContext;
+    imagePickerController.sourceType = sourceType;
+    imagePickerController.delegate = self;
+    
+    if (sourceType == UIImagePickerControllerSourceTypeCamera) {
+        /*
+         The user wants to use the camera interface. Set up our custom overlay view for the camera.
+         */
+        imagePickerController.showsCameraControls = NO;
+        
+        /*
+         Load the overlay view from the OverlayView nib file. Self is the File's Owner for the nib file, so the overlayView outlet is set to the main view in the nib. Pass that view to the image picker controller to use as its overlay view, and set self's reference to the view to nil.
+         */
+        [[NSBundle mainBundle] loadNibNamed:@"OverlayView" owner:self options:nil];
+        self.overlayView.frame = imagePickerController.cameraOverlayView.frame;
+        imagePickerController.cameraOverlayView = self.overlayView;
+        self.overlayView = nil;
+    }
+    
+    self.imagePickerController = imagePickerController;
+    [self presentViewController:self.imagePickerController animated:YES completion:nil];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,5 +63,8 @@
     // Pass the selected object to the new view controller.
 }
 */
+- (IBAction)takePictureButtonPressed:(id)sender {
+    [self.imagePickerController takePicture];
+}
 
 @end
